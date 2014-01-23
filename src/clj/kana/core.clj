2e1a8effdb5d7113a2a4d133a664025f1cf39a34
Anders Eliasson <anders.eliasson@digitalroute.com>
2014-01-23 18:37:48 +0100

    added katakana

--- src/clj/kana/core.clj
+++ src/clj/kana/core.clj
@@ -14,11 +14,21 @@
 
 
 
+(defmacro dbg [body]
+  `(let [x# ~body]
+     (println "dbg:" '~body "=" x#)
+     x#))
+
+
 (defn max-length [l] 
   (->> (map #(.-length %) l) (apply max)))
 
 (def max-length-memo 
   (memoize max-length))
+
+(defn reverse-map [m]
+ (apply hash-map (mapcat reverse (filter (fn [[_ v]] v) (partition 2 m)))))
+
   
 (defn kana-grammar [alphabet vocabulary]
   (let [l-fn 
@@ -39,6 +49,9 @@
 	        (recur (.substring s (.-length k)) (conj res k)))))))
 
 
+(def vowels #{"a" "i" "u" "e" "o"})
+
+(def consonants #{"k" "s" "z" "t" "d" "h" "b" "p" "m" "y" "r" "w" "sh" "ch" "n"})
 
 
 (def kana-vocabulary
@@ -170,29 +183,160 @@
    "sho"  "\u3057\u3087"
    ])
 
+(def alphabet-katakana
+  [
+   "a" "\u30a2"
+   "i" "\u30a4"
+   "u" "\u30a6"
+   "e" "\u30a8"
+   "o" "\u30aa"
+
+   "ka" "\u30ab"
+   "ki" "\u30ad"
+   "ku" "\u30af"
+   "ke" "\u30a1"
+   "ko" "\u30a3"
+
+   "ga" "\u30ac"
+   "gi" "\u30ae"
+   "gu" "\u30a0"
+   "ge" "\u30a2"
+   "go" "\u30a4"
+
+
+   "sa" "\u30b5"
+   "shi" "\u30b7"
+   "su" "\u30b9"
+   "se" "\u30bb"
+   "so" "\u30bd"
+
+   "za" "\u30b6"
+   "zi" "\u30b8"
+   "zu" "\u30ba"
+   "ze" "\u30bc"
+   "zo" "\u30be"
+
+   "ta" "\u30bf"
+   "chi" "\u30b1"
+   "tu" "\u30b4"
+   "te" "\u30b6"
+   "to" "\u30b8"
+
+   "da" "\u30c0"
+   "di" "\u30c2"
+   "du" "\u30c5"
+   "de" "\u30c7"
+   "do" "\u30c9"
+
+   "na" "\u30ca"
+   "ni" "\u30cb"
+   "nu" "\u30cc"
+   "ne" "\u30cd"
+   "no" "\u30ce"
+
+   "ha" "\u30cf"
+   "hi" "\u30c2"
+   "hu" "\u30c5"
+   "he" "\u30c8"
+   "ho" "\u30cb"
+
+   "ba" "\u30d0"
+   "bi" "\u30d3"
+   "bu" "\u30d6"
+   "be" "\u30d9"
+   "bo" "\u30dc"
+
+   "pa" "\u30d1"
+   "pi" "\u30d4"
+   "pu" "\u30d7"
+   "pe" "\u30da"
+   "po" "\u30dd"
+
+
+   "ma" "\u30de"
+   "mi" "\u30df"
+   "mu" "\u30e0"
+   "me" "\u30e1"
+   "mo" "\u30e2"
+
+   "ya" "\u30e4"
+   "yi" nil
+   "yu" "\u30e6"
+   "ye" nil
+   "yo" "\u30e8"
+
+   "ra" "\u30e9"
+   "ri" "\u30ea"
+   "ru" "\u30eb"
+   "re" "\u30ec"
+   "ro" "\u30ed"
+
+   "wa" "\u30ef"
+   "wi" nil
+   "wu" "\u3090"
+   "we" "\u3091"
+   "wo" "\u3092"
+
+   "n" "\u30f3"
+   "nni" nil
+   "nnu" nil
+   "nne" nil
+   "nno" nil
+
+   "sha" "\u30b7\u30e3"
+   "_shi" nil
+   "shu"  "\u30b7\u30e5"
+   "_she" nil
+   "sho"  "\u30b7\u30e7"
+   ])
+
 
 (def alphabet-to-hiragana-map
   (apply hash-map alphabet-hiragana)
   )
+
 (def hiragana-to-alphabet-map
- (apply hash-map (mapcat reverse (filter (fn [[_ v]] v) (partition 2 alphabet-hiragana)))))
+  (reverse-map alphabet-hiragana))
 
 (def hiragana-vocabulary-as-set (into #{} (keys hiragana-to-alphabet-map)))
 
+(def alphabet-to-katakana-map
+  (apply hash-map alphabet-katakana)
+  )
 
-(def vowels #{"a" "i" "u" "e" "o"})
+(def katakana-to-alphabet-map
+  (reverse-map alphabet-katakana))
 
-(def consonants #{"k" "s" "z" "t" "d" "h" "b" "p" "m" "y" "r" "w" "sh" "ch" "n"})
+(def katakana-vocabulary-as-set (into #{} (keys katakana-to-alphabet-map)))
+
 
 (defn translate [word vocabulary voc-map] (->> (kana-grammar word vocabulary) (map #(voc-map %)) (reduce str)))
 
 
-(defn hiragana-of [alphabet-word]
-  (translate alphabet-word kana-vocabulary-as-set alphabet-to-hiragana-map)
+(defn alphabet-of [word]
+  (let [parts (partition-by 
+                #(let [s (str %)]
+                   (cond (contains? hiragana-vocabulary-as-set s) 1
+                         (contains? katakana-vocabulary-as-set s) 2
+                         :else 0)) 
+                word)]
+    (apply str (map #(let [s (apply str %)
+                           ch (.substring s 0 1)]
+                       (cond 
+                         (contains? hiragana-vocabulary-as-set ch)
+                         (translate s hiragana-vocabulary-as-set hiragana-to-alphabet-map)
+                         (contains? katakana-vocabulary-as-set ch)
+                         (translate s katakana-vocabulary-as-set katakana-to-alphabet-map)
+                         :else 
+                         s))
+                         parts))))
+
+(defn hiragana-of [word]
+  (translate (to-alphabet word) kana-vocabulary-as-set alphabet-to-hiragana-map)
   )
 
-(defn alphabet-of [hiragana-word]
-  (translate hiragana-word hiragana-vocabulary-as-set hiragana-to-alphabet-map)
+(defn katakana-of [word]
+  (translate (to-alphabet word) kana-vocabulary-as-set alphabet-to-katakana-map)
   )
 
 ;;------------------------------------------------------------------------------------
